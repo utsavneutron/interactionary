fcaeab0b66b223ba47a6fc2b3268dd34d582a949
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2020-09-23 22:57:23 -0500

    Added get_close_matches

--- Dictionary.py
+++ Dictionary.py
@@ -9,7 +9,7 @@ def search(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead" % get_close_matches(word, data.keys())[0]
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
     else:
         return "The word doesn't exist. Please double check it"
 
