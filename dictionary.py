7277e473a3ff0dfcdf92dae4ce6aac71b562ec85
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2020-09-23 23:11:28 -0500

    commit by Master

--- dictionary.py
+++ dictionary.py
@@ -9,7 +9,14 @@ def search(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+        yes = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
+                    get_close_matches(word, data.keys())[0])
+        if yes == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yes == "N":
+            return "The Word is not in the dictionary. Are you sure it's not a slang?"
+        else:
+            return "We didn't undestand the word"
     else:
         return "The word doesn't exist. Please double check it"
 
@@ -18,7 +25,7 @@ word = ""
 
 while (word != "/end"):
     word = input("Enter a word: ")
-    print(search(word))
-
     if(word == "/end"):
         print("Thank you for using the dictionary")
+        break
+    print(search(word))
